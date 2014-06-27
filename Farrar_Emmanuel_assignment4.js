dcc1d041ceca7b11d333ed5cbd20f49087443e47
Emmanuel Farrar <emmanuel.farrar@fullsail.edu>
2014-06-27 04:47:02 +0430

    Change 6
    Added: return variables
    Cleaned up function statements to comply with guidance

--- Farrar_Emmanuel_assignment4.js
+++ Farrar_Emmanuel_assignment4.js
@@ -3,6 +3,11 @@
 //SDI - 1406 - Week 4 Assignment
 
 //global variable
+var phoneNumReturn;
+var validEmailReturn;
+var validUrlReturn;
+var decimalPlaceReturn;
+var strToNumReturn
 var wordCaseReturn;
 
 
@@ -17,19 +22,14 @@ var thePhoneNum = function (phoneStr)
 	var firstDash = phoneStr.charAt(3);
 	var secondDash = phoneStr.charAt(7);
 	
-	console.log("The phone number is " + phoneStr);
-	console.log("The length of this number is " +phoneStr.length);
-	
 	if (lengthOfPhone === 12 && firstDash === "-" && secondDash ==="-")
 	{
-		console.log("This is a valid phone number")
+		console.log("The number " +phoneStr +" is a valid phone number.")
 		return true;
 	}else {
-		console.log("Not a valid phone number.");
+		console.log("The number " +phoneStr +" is not Not a valid phone number.");
 		return false;
 	};
-	
-	return phoneStr;
 };
 
 //Does the string follow aaa@bbb.ccc pattern like an email address
@@ -43,9 +43,11 @@ var validEmail = function (myEmail)
 	//using the redefined variable to identify the "." and a way to identify the "@"
 	if (myDotCom[0] ==="." && foundAtPosition != -1)
 	{
-		console.log ("Valid Email");
+		console.log ("The email address " +myEmail +" is a valid email address.");
+		return true;
 		} else {
 		console.log("Next time try a valid email");
+		return false;
 		}
 	
 };
@@ -59,9 +61,11 @@ var validUrl = function (enteredUrl)
 	
 	if (subValidUrl === "http" ||subValidUrl2 === "https")
 		{
-		console.log("Congrats");
+		console.log("This is a proper URL heading");
+		return true;
 		}else{
-		console.log("Loser")
+		console.log("This is not a proper URL heading");
+		return false;
 		}
 	
 };
@@ -80,29 +84,30 @@ var stringToNumber = function (strNumber)
 var theString;
 theString = parseInt(strNumber);
 console.log("The number is " + theString);
+return theString;
 };
 
 //Title-case a string (split into words, then uppercase the first letter of each word).
 var wordCase = function (someString)
 {
-var firstLetter = someString.substring(0,1).toUpperCase();
-var secondCap = someString.substring(5,6).toUpperCase();
+var firstLetter = someString.substring(0,1).toUpperCase() +someString.substring(1,4);
+var secondCap = someString.substring(4,5).toUpperCase() +someString.substring(5);
 console.log(firstLetter + secondCap);
-return firstLetter;
+return firstLetter.toUpperCase() + someString.substring(1);
 };
 
 //main code
 
 //function call for thePhoneNum
-thePhoneNum("123-456-7890");
+phoneNumReturn = thePhoneNum("123-456-7890");
 //function call for the validEmail function
-validEmail("javascript@gmail.com");
+validEmailReturn = validEmail("javascript@gmail.com");
 //function call for the validUrl function
-validUrl("http://nike.com");
-//fucntion call for decimalPlace function
-decimalPlace(10.15552, 2);
+validUrlReturn = validUrl("http://nike.com");
+//function call for decimalPlace function
+decimalPlaceReturn = decimalPlace(10.15552, 2);
 //function call for isThisANum
-stringToNumber("250");
+strToNumReturn = stringToNumber("250");
 //function call for wordCase function
 wordCaseReturn = wordCase("bed time");
 
